e807b7f6e52fbedf28e938a2ee4360da9d4ffc0f
Hasan Majid <hasanmajid3.14@hotmail.com>
2022-11-22 21:53:34 -0500

    deploying test

---
+++ ssr-server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.get("/movie", (req, res) => {
+      return res.end("we made it!");
+    });
+
+    server.get("/posts", (req, res) => {
+    //   const actualPage = "/post";
+    //   const queryParams = req.params.id;
+      res.end("<h1>Hello!</h1>");
+    });
+
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(3000, (err) => {
+      if (err) throw err;
+      console.log("> Ready on http://localhost:3000");
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
